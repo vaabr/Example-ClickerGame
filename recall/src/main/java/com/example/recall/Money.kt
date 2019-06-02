c4528a57c8f56127785bee9c2bbf266e3b133135
Wadimabr <wadimabr@users.noreply.github.com>
2019-06-02 19:48:50 +0200

    fix pls

--- recall/src/main/java/com/example/recall/Money.kt
+++ recall/src/main/java/com/example/recall/Money.kt
@@ -6,36 +6,55 @@ import kotlin.math.abs
 object Money {
     var money = 0
     var multiplier = 0
-    val moneyStack = Stack<Int>()
+    var moneyStack = Stack<Int>()
 
-    fun countMultiplier(value: Int): Int{
+    fun countMultiplier(value: Int): Int {
         var mult = 0
-        for(g in 1 until abs(value).toString().length){
-            if(g%3==0) mult++
+        for (g in 1 until abs(value).toString().length) {
+            if (g % 3 == 0) mult++
         }
         return mult
     }
 
-    fun disassemble(value: Int): Array<Int?>{
+    fun disassemble(value: Int): Array<Int?> {
         var num = value
-        val mult = countMultiplier(num)+1
+        val mult = countMultiplier(num) + 1
         val array = arrayOfNulls<Int>(mult)
-        for (i in 0 until mult){
-            array[i]=num%1000
+        for (i in 0 until mult) {
+            array[i] = num % 1000
             num /= 1000
         }
         return array
     }
 
-    fun packToStack(array: Array<Int?>): Stack<Int>{
+    fun packToStack(array: Array<Int?>): Stack<Int> {
         val stack = Stack<Int>()
-        for(i in array.size until 0){
+        for (i in array.size until 0) {
             stack.push(array[i])
         }
         return stack
     }
 
-    fun increaseMoney(amount: Int){
+    fun getMoneyFromStack(): Int {
+        if (moneyStack.isEmpty()) {
+            val array = disassemble(money)
+            moneyStack = packToStack(array)
+        }
+        return moneyStack.peek()
+    }
+
+    fun getValueFromStack(stack: Stack<Int>): Int{
+        return stack.peek()
+    }
+
+    fun getValueFromEmptyStack(stack: Stack<Int>, value: Int): Int{
+        var stackVar = stack
+        val array = disassemble(value)
+        stackVar = packToStack(array)
+        return stackVar.peek()
+    }
+
+    fun increaseMoney(amount: Int) {
         Functions.doNothing()
     }
 }
--- recall/src/test/java/com/example/recall/MoneyTest.kt
+++ recall/src/test/java/com/example/recall/MoneyTest.kt
@@ -2,15 +2,20 @@ package com.example.recall
 
 import com.example.recall.Money.countMultiplier
 import com.example.recall.Money.disassemble
+import com.example.recall.Money.getMoneyFromStack
+import com.example.recall.Money.getValueFromEmptyStack
+import com.example.recall.Money.getValueFromStack
+import com.example.recall.Money.money
 import com.example.recall.Money.moneyStack
 import com.example.recall.Money.packToStack
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.util.*
 
 class MoneyTest {
 
     @Test
-    fun countMultiplier_isCorrect(){
+    fun countMultiplier_isCorrect() {
         assertEquals(0, countMultiplier(0))
         assertEquals(1, countMultiplier(1000))
         assertEquals(1, countMultiplier(-5000))
@@ -25,7 +30,7 @@ class MoneyTest {
     }
 
     @Test
-    fun disassemble_isCorrect(){
+    fun disassemble_isCorrect() {
         var array = disassemble(5000)
         assertEquals(0, array[0])
         assertEquals(5, array[1])
@@ -36,7 +41,7 @@ class MoneyTest {
     }
 
     @Test
-    fun moneyStack_isCorrect(){
+    fun moneyStack_isCorrect() {
         moneyStack.push(1)
         moneyStack.push(2)
         moneyStack.push(3)
@@ -46,16 +51,66 @@ class MoneyTest {
     }
 
     @Test
-    fun packToStack_isCorrect(){
-        val testArray = arrayOf(0,1,2,3,4,5,6,7,8,9)
+    fun packToStack_isCorrect() {
+        val testArray = arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
         val testNullsArray = arrayOfNulls<Int>(testArray.size)
-        for(i in 0 until testArray.size){
-            testNullsArray[i]=testArray[i]
+        for (i in 0 until testArray.size) {
+            testNullsArray[i] = testArray[i]
         }
         val testStack = packToStack(testNullsArray)
-        for(i in 0 until testStack.size){
+        for (i in 0 until testStack.size) {
             assertEquals(i, testStack.pop())
         }
         assertEquals(true, testStack.isEmpty())
+    }
+
+    @Test
+    fun getMoneyFromStack_isCorrect() {
+        money = 15700
+        assertEquals(15, getMoneyFromStack())
+        assertEquals(15, getMoneyFromStack())
+        assertEquals(15, moneyStack.pop())
+        assertEquals(700, getMoneyFromStack())
+    }
+
+    @Test
+    fun getValueFromStack_isCorrect(){
+        val stack = Stack<Int>()
+        for(i in 0 until 10) {
+            stack.push(i)
+        }
+        assertEquals(9, getValueFromStack(stack))
+        assertEquals(9, getValueFromStack(stack))
+    }
+
+    @Test
+    fun getValueFromEmptyStack_isCorrect(){
+        val stack = Stack<Int>()
+        val value = 15700
+        assertEquals(15, getValueFromEmptyStack(stack, value))
+        assertEquals(15, getValueFromEmptyStack(stack, value))
+    }
+
+    @Test
+    fun somTest(){
+        val stack = Stack<Int>()
+        assertEquals(true, stack.isEmpty())
+        val stack2 = Stack<Int>()
+        stack2.push(0)
+        assertEquals(false, stack2.isEmpty())
+    }
+
+    @Test
+    fun getValueFromEmptyStack_Test(){
+        var value = 15700
+        var testStack: Stack<Int>
+        val array = disassemble(value)
+        assertEquals(700, array[0])
+        assertEquals(15, array[1])
+        testStack = packToStack(array)
+        assertEquals(false, testStack.isEmpty())
+        assertEquals(15, testStack.peek())
+        value = testStack.peek()
+        assertEquals(15, value)
     }
 }
