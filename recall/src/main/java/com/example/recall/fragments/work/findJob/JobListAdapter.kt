da3294745ef689011971c4e29977e7da3764e5d1
Wadimabr <46972984+wadimabr@users.noreply.github.com>
2019-09-23 12:00:03 +0200

    upd

--- recall/src/main/java/com/example/recall/fragments/work/findJob/JobListAdapter.kt
+++ recall/src/main/java/com/example/recall/fragments/work/findJob/JobListAdapter.kt
@@ -1,17 +1,26 @@
 package com.example.recall.fragments.work.findJob
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recall.fragments.work.Job
+import com.example.recall.fragments.work.JobsObject
+import com.example.recall.fragments.work.JobsObject.KEY_BEGGAR
 
-class JobListAdapter(val jobs: Map<String, Job>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class JobListAdapter(val jobs: Map<String, Job>): RecyclerView.Adapter<JobListViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JobListViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return JobListViewHolder(inflater, parent)
+    }
+
+    override fun onBindViewHolder(holder: JobListViewHolder, position: Int) {
+        val job: Job = jobs[JobsObject.arrayOfKeys[position]] ?: jobs[KEY_BEGGAR]!!
+        holder.bind(job)
+    }
+
     override fun getItemCount(): Int = jobs.size
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+
+
 }
--- recall/src/main/java/com/example/recall/fragments/work/findJob/JobListViewHolder.kt
+++ recall/src/main/java/com/example/recall/fragments/work/findJob/JobListViewHolder.kt
@@ -1,8 +1,41 @@
 package com.example.recall.fragments.work.findJob
 
-import android.view.View
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recall.R
+import com.example.recall.fragments.work.Job
+import com.example.recall.fragments.work.JobsObject
 
-class JobListViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+class JobListViewHolder(inflater: LayoutInflater, parent: ViewGroup) : RecyclerView.ViewHolder(
+    inflater.inflate(
+        R.layout.jobslist_item, parent, false
+    )
+) {
+    private var mJobIcon: ImageView? = null
+    private var mJobName: TextView? = null
+    private var mJobDescription: TextView? = null
+    private var mApply: Button? = null
+    private var mSalary: TextView? = null
+
+    init {
+        mJobIcon = itemView.findViewById(R.id.iv_jobIcon)
+        mJobName = itemView.findViewById(R.id.tv_jobName)
+        mJobDescription = itemView.findViewById(R.id.tv_jobDescription)
+        mApply = itemView.findViewById(R.id.b_apply)
+        mSalary = itemView.findViewById(R.id.tv_salary)
+    }
+
+    fun bind(job: Job) {
+        mJobIcon?.setImageResource(job.iconID)
+        mJobName?.text = job.name
+        mJobDescription?.text = job.description
+        mApply?.setOnClickListener { JobsObject.setCurrent(JobsObject.jobsMap.filterValues { it == job }.keys.first()) }
+        val string = "${job.salary}$"
+        mSalary?.text = string
+    }
 
 }
